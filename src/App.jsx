e5129eb9bd3e220ec1598037fb9634891cd42eae
kaar-codes <webdesignkaar@gmail.com>
2026-03-07 15:32:18 +0530

    Login Image Updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <NavBar />
+        {/* <NavBar /> */}
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,18 @@
 function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <section className="flex flex-col justify-center align-bottom bg-[#916BBF] w-1/2 h-screen">
+        <div>
+          <h1 className="text-4xl">SPR Catering</h1>
+          <p>Welcome to SPR</p>
+          <p>Join us now!</p>
+        </div>
+        <img
+          src="mobile-login-mockup.svg"
+          alt="login or register mobile mockup"
+          className="w-130"
+        />
+      </section>
     </>
   );
 }
